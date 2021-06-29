ae08877b800b4ae25953e07fc3d92b4f9dbc2a1f
aayushrishi <aayushrishi09@gmail.com>
2021-06-30 01:50:05 +0530

    code fixes

--- test/unit_test_cases.py
+++ test/unit_test_cases.py
@@ -3,11 +3,15 @@ import unittest
 
 class UnitTest_Salary(unittest.TestCase):
 
-    def run_test(self):
+    def test_run(self):
         salaryCalc = Salary_Calculation.Salary(10000, 10, 5)
 
+        self.assertEqual(salaryCalc.calculatePF(), 1000, "PASS")
         self.assertNotEqual(salaryCalc.calculatePF(), 500, "PASS")
-        self.assertTrue(salaryCalc.calculateInhandSalary(), 500, "PASS")
-        self.assertFalse(salaryCalc.calculatePF(), 500, "PASS")
-        self.assertGreater(salaryCalc.calculatePF(), 500, "PASS")
-        self.assertLess(salaryCalc.calculatePF(), 500, "PASS")
+        self.assertTrue(salaryCalc.calculatePension() == 500, "PASS")
+        self.assertFalse(salaryCalc.calculatePension() == 1000, "PASS")
+        self.assertGreater(salaryCalc.calculateInhandSalary(), 5000, "PASS")
+        self.assertLess(salaryCalc.calculateInhandSalary(), 10000, "PASS")
+
+if __name__ == '__main__':
+    unittest.main()
